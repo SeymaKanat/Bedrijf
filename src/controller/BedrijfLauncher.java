8f8645f5ae4256c0f660281fd05aa63a941014f2
Seyma Kanat <knt.seyma@gmail.com>
2023-10-03 14:45:04 +0200

    8.1 Bedrijf

--- src/controller/BedrijfLauncher.java
+++ src/controller/BedrijfLauncher.java
@@ -4,6 +4,7 @@ import model.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 /**
  * @author Seyma Kanat <s.kanat@st.hanze.nl>
@@ -11,65 +12,103 @@ import java.util.Collections;
  */
 public class BedrijfLauncher {
     public static void main(String[] args) {
-        Afdeling[] afdelingen = new Afdeling[4];
-        afdelingen[0] = new Afdeling("Uitvoering", "Hilversum");
-        afdelingen[1] = new Afdeling("Support", "Amsterdam");
-        afdelingen[2] = new Afdeling("Management", "Almere");
-        afdelingen[3] = new Afdeling("Documentatie", "Gouda");
-        ArrayList<Persoon> personen = new ArrayList<>();
-        personen.add(new Werknemer("Mark", "Den Haag", afdelingen[2], 10000));
-        personen.add(new Werknemer("Angelique", "Rotterdam", afdelingen[2], 5000));
-        personen.add(new Werknemer("Caroline", "Delft", afdelingen[1], 4000));
-        personen.add(new Zzper("Klaas", "Diemen", afdelingen[3], 50.00));
-        personen.add(new Zzper("Ronald", "Zaandam", afdelingen[0], 80.00));
-        personen.add(new Zzper("Jannie", "Utrecht", afdelingen[0], 60.00));
-        personen.add(new Zzper("Anne", "Zwolle", afdelingen[0], 40.00));
-        personen.add(new Vrijwilliger("Ambi", "Amsterdam", afdelingen[0]));
-        personen.add(new Vrijwilliger("Naledi", "Gaborone", afdelingen[1]));
-        personen.add(new Vrijwilliger("Ceren", "Istanboel", afdelingen[2]));
-        personen.add(new Vrijwilliger("Haining", "Shaoxing", afdelingen[3]));
-        //6.5) Gebruik een for-loop met instanceof en typecasting om alle zzp-ers in de arraylist voor 320
-        //uur in te huren.
 
-        for (Persoon persoon : personen) {
-            if (persoon instanceof Zzper) {
-                ((Zzper) persoon).huurIn(320);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Geef de naam: ");
+        String naam = input.nextLine();
+
+        System.out.print("Geef de woonplaats: ");
+        String woonplaats = input.nextLine();
+
+        System.out.print("Geef de naam van de afdeling: ");
+        String afdelingNaam = input.nextLine();
+
+        System.out.print("Geef de plaats van de afdeling: ");
+        String afdelingPlaats = input.nextLine();
+
+        Afdeling afdeling = new Afdeling(afdelingNaam, afdelingPlaats);
+        Werknemer werknemer = null;
+
+        while (werknemer == null) {
+            System.out.print("Geef het maandsalaris: ");
+            double maandSalaris = input.nextDouble();
+
+            try {
+                werknemer = new Werknemer(naam, woonplaats,afdeling, maandSalaris);
+                System.out.println(werknemer);
+            } catch (IllegalArgumentException IllegalArgumentException) {
+                System.out.println(IllegalArgumentException.getMessage());
+            } finally {
+                System.out.println("Je invoer is op de juiste wijze afgehandeld.");
+
+
             }
-
-        }//7.6.b.
-        for (Persoon persoon : personen) {
-            if (persoon instanceof Zzper) {
-                ((Zzper) persoon).huurIn(320);
-            } else if (persoon instanceof Vrijwilliger) {
-                ((Vrijwilliger) persoon).huurIn(160);
-            }
-
-        }
-        for (Persoon persoon : personen) {
-            if (persoon instanceof Oproepbaar) {
-                ((Oproepbaar) persoon).huurIn(320);
-            }
-            if (persoon instanceof Zzper) {
-                ((Zzper) persoon).huurIn(160);
-            }//vraag!! dit is opsie
-
-
-        }
-        //7.6. Sorteer de ArrayList op alfabetische volgorde
-        Collections.sort(personen);
-
-        for (Persoon persoon : personen) {
-            System.out.println(persoon);
-            toonJaarInkomen(persoon);
         }
 
     }
-
-    public static void toonJaarInkomen (Persoon persoon){
-        System.out.printf("%s verdient %.2f per jaar\n", persoon.getNaam(), persoon.berekenJaarInkomen());
-    }
-
 }
+
+
+//        Afdeling[] afdelingen = new Afdeling[4];
+//        afdelingen[0] = new Afdeling("Uitvoering", "Hilversum");
+//        afdelingen[1] = new Afdeling("Support", "Amsterdam");
+//        afdelingen[2] = new Afdeling("Management", "Almere");
+//        afdelingen[3] = new Afdeling("Documentatie", "Gouda");
+//        ArrayList<Persoon> personen = new ArrayList<>();
+//        personen.add(new Werknemer("Mark", "Den Haag", afdelingen[2], 10000));
+//        personen.add(new Werknemer("Angelique", "Rotterdam", afdelingen[2], 5000));
+//        personen.add(new Werknemer("Caroline", "Delft", afdelingen[1], 4000));
+//        personen.add(new Zzper("Klaas", "Diemen", afdelingen[3], 50.00));
+//        personen.add(new Zzper("Ronald", "Zaandam", afdelingen[0], 80.00));
+//        personen.add(new Zzper("Jannie", "Utrecht", afdelingen[0], 60.00));
+//        personen.add(new Zzper("Anne", "Zwolle", afdelingen[0], 40.00));
+//        personen.add(new Vrijwilliger("Ambi", "Amsterdam", afdelingen[0]));
+//        personen.add(new Vrijwilliger("Naledi", "Gaborone", afdelingen[1]));
+//        personen.add(new Vrijwilliger("Ceren", "Istanboel", afdelingen[2]));
+//        personen.add(new Vrijwilliger("Haining", "Shaoxing", afdelingen[3]));
+//        //6.5) Gebruik een for-loop met instanceof en typecasting om alle zzp-ers in de arraylist voor 320
+//        //uur in te huren.
+//
+//        for (Persoon persoon : personen) {
+//            if (persoon instanceof Zzper) {
+//                ((Zzper) persoon).huurIn(320);
+//            }
+//
+//        }//7.6.b.
+//        for (Persoon persoon : personen) {
+//            if (persoon instanceof Zzper) {
+//                ((Zzper) persoon).huurIn(320);
+//            } else if (persoon instanceof Vrijwilliger) {
+//                ((Vrijwilliger) persoon).huurIn(160);
+//            }
+//
+//        }
+//        for (Persoon persoon : personen) {
+//            if (persoon instanceof Oproepbaar) {
+//                ((Oproepbaar) persoon).huurIn(320);
+//            }
+//            if (persoon instanceof Zzper) {
+//                ((Zzper) persoon).huurIn(160);
+//            }//vraag!! dit is opsie
+//
+//
+//        }
+//        //7.6. Sorteer de ArrayList op alfabetische volgorde
+//        Collections.sort(personen);
+//
+//        for (Persoon persoon : personen) {
+//            System.out.println(persoon);
+//            toonJaarInkomen(persoon);
+//        }
+//
+//    }
+//
+//    public static void toonJaarInkomen (Persoon persoon){
+//        System.out.printf("%s verdient %.2f per jaar\n", persoon.getNaam(), persoon.berekenJaarInkomen());
+//    }
+
+
 
 
 
--- src/model/Werknemer.java
+++ src/model/Werknemer.java
@@ -7,14 +7,17 @@ package model;
 public class Werknemer extends Persoon {
     private static final double DEFAULT_MAAND_SALARIS = 0.0;
     private static final int MAANDEN_PER_JAAR = 12;
+    private static final int MAAND_SALARIS = 0;
     private static double GRENSWAARDE_BONUS = 4500.00;
     private double maandSalaris;
 
     //Constructors
-    public Werknemer(String naam, String woonplaats, Afdeling afdeling,double maandSalaris) {
+    public Werknemer(String naam, String woonplaats, Afdeling afdeling, double maandSalaris) {
         super(naam, woonplaats, afdeling);
-        this.maandSalaris = maandSalaris;
+        setMaandsalaris(maandSalaris);
     }
+
+
 
     public Werknemer(String naam) {
         super(naam);
@@ -46,23 +49,19 @@ public class Werknemer extends Persoon {
 
         return jaarinkomen;
     }
-    @Override
+    @Override //8.5
     public String toString() {
         return String.format("%s en is een werknemer %s recht op bonus",
                 super.toString(), heeftRechtOpBonus() ? "met" : "zonder");
     }
 
-
-    private void setMaandsalaris(double maandSalaris) {
+    private void setMaandsalaris(double maandSalaris) { //8.2
         if (maandSalaris < 0) {
-            System.err.printf("Het maandsalaris mag niet negatief zijn! Het maandsalaris wordt op %.1f gezet.\n",
-                    DEFAULT_MAAND_SALARIS);
-            this.maandSalaris = DEFAULT_MAAND_SALARIS;
-
-        } else {
+            throw new IllegalArgumentException("Het maandsalaris mag niet negatief zijn"); //8.2.a.Voeg de methode setMaandsalaris toe
+        }
             this.maandSalaris = maandSalaris;
         }
     }
-}
+
 
 
